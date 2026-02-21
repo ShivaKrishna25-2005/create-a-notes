4d630819f630b11c9a08e86ff78b34a9765bda6b
Yadavalli Shiva Krishna <yadavallishivakrishna@gmail.com>
2026-02-21 22:54:23 +0530

    Update Day8_create notes file.py

--- Day8_create notes file.py
+++ Day8_create notes file.py
@@ -13,3 +13,12 @@ print("File created and content written successfully!.\n")
 with open("journey.txt","r")as file:
     print(file.read())
     #print(file.readline())///  By using this line we have to see the content in single line
+
+
+#OUTPUT:
+#File created and content written successfully!.
+#I started learning AI with curiosity.
+#I began with Python basics.
+#I practiced small coding tasks daily.
+#I learned about AI agents and automation.
+#Now I am building mini AI projects.'''
