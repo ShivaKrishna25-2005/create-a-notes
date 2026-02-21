54e31b5bb85e2e0894c7173678246c9a17b05ca5
Yadavalli Shiva Krishna <yadavallishivakrishna@gmail.com>
2026-02-21 22:52:00 +0530

    Add files via upload

---
+++ Day8_create notes file.py
@@ -0,0 +1,15 @@
+# Creates Notes File by using file handling:
+
+file = open("journey.txt", "w")
+file.write("I started learning AI with curiosity.\n")#create a file with name journey.txt.
+#write 5 lines about my AI learning jrny.
+file.write("I began with Python basics.\n")
+file.write("I practiced small coding tasks daily.\n")
+file.write("I learned about AI agents and automation.\n")
+file.write("Now I am building mini AI projects.\n")
+#After finishing the contwnt writen must and should close the file.
+file.close()
+print("File created and content written successfully!.\n")
+with open("journey.txt","r")as file:
+    print(file.read())
+    #print(file.readline())///  By using this line we have to see the content in single line
